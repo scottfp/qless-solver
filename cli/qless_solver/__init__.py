bf4a5e0dabae5151b801b64a665eb206852250b8
Scott Pritchard <scottfp@users.noreply.github.com>
2025-06-07 15:37:57 -0400

    Add package version constant

--- cli/qless_solver/__init__.py
+++ cli/qless_solver/__init__.py
@@ -1,9 +1,12 @@
-# This file makes Python treat the directory qless_solver as a package.
+"""Package initialization for :mod:`qless_solver`."""
+
+# Expose the package version for other modules and the CLI
+__version__ = "0.1.0"
+
+__all__ = ["__version__"]
 
 # Optionally, you can make specific classes or functions available for easier import, e.g.:
 # from .grid_solver import Grid, PlacedWord, GridPosition, solve_qless_grid
-# from .solver import solve_qless # Example if you have other solvers
+# from .solver import solve_qless  # Example if you have other solvers
 # from .dictionary import Dictionary
-# from .dice import Dice # etc.
-
-# For now, leave it empty or with comments, specific exports can be added as needed.
+# from .dice import Dice  # etc.
